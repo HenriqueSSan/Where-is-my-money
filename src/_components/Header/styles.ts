9d68d073f2b18ca86e60af16de150c5a8fe518f3
HenriqueSSan <hsstudentwork@gmail.com>
2022-07-07 17:00:13 -0300

    feat: Responsive devices

--- src/_components/Header/styles.ts
+++ src/_components/Header/styles.ts
@@ -1,30 +1,39 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const Container = styled.header`
   background: var(--blue);
-`
+`;
 
 export const Content = styled.div`
-  max-width:1120px;
-  margin:0 auto;
+  max-width: 100%;
+  margin: 0 auto;
   padding: 2rem 1rem 12rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
 
-  button{
+  @media screen and (min-width:1023px){
+    max-width: 1120px;
+  }
+
+  button {
     font-size: 1rem;
     color: #fff;
     background: var(--blue-light);
     padding: 0 2rem;
-    border:0;
-    border-radius: .25rem;
+    border: 0;
+    border-radius: 0.25rem;
     height: 3rem;
-    transition: filter .3s ease-in;
-    
-    &:hover{
-      filter: brightness(.6);
+    transition: filter 0.3s ease-in;
+
+    &:hover {
+      filter: brightness(0.6);
     }
   }
-`
+  @media screen and (max-width:768px){
+    img{
+      width: 45%;
+    }
+  }
+
+`;
--- src/_features/Dashboard/Summary/styles.ts
+++ src/_features/Dashboard/Summary/styles.ts
@@ -1,33 +1,41 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(3,1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
   margin-top: -9rem;
+  overflow: auto;
 
-  div{
+  @media screen and (min-width: 1023px) {
+    
+  }
+
+  div {
     background: var(--shape);
     padding: 1.5rem 2rem;
-    border-radius: .25rem;
+    border-radius: 0.25rem;
     color: var(--text-title);
 
-    header{
+    @media screen and (max-width: 769px) {
+      width: 260px;
+    }
+
+    header {
       display: flex;
       align-items: center;
       justify-content: space-between;
-
     }
-    strong{
+    strong {
       margin-top: 1rem;
       font-size: 2rem;
       font-weight: 500;
       line-height: 3rem;
     }
 
-    &.highlight-background{
+    &.highlight-background {
       background-color: var(--green);
       color: #fff;
     }
   }
-`
+`;
